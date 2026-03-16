c06210fc9c6240a788c38c2c5ea18c208202e22b
sorious77 <sorious77@naver.com>
2026-03-16 20:17:37 +0900

    :sparkles: gradle 설정 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,13 +36,14 @@ subprojects {
     dependencies {
         implementation("org.springframework.boot:spring-boot-starter")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
-        testImplementation("org.springframework.boot:spring-boot-starter-test")
-        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
         testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 
         implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-        testImplementation("io.kotest:kotest-assertions-core:6.1.7")
+        testImplementation("org.springframework.boot:spring-boot-starter-test")
+        testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+        testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+        testImplementation("io.kotest:kotest-assertions-core:5.9.1")
         testImplementation("io.mockk:mockk:1.14.9")
     }
 
